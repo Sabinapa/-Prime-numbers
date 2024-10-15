5ff4bfe50fe66edd19eba9c403e882ba559fe17e
sabina <sabina.pauric@student.um.si>
2024-10-15 21:11:04 +0200

    Added the naive method for generating prime numbers, utilizing the implemented LCG and RANDOM functions for generating random n-digit numbers.

--- main.cpp
+++ main.cpp
@@ -16,6 +16,38 @@ uint64_t RANDOM(uint64_t a, uint64_t b) {
     return a + (LCG(a,b) % (b - a + 1));
 }
 
+uint64_t generateRandomNumber(int n) {
+    uint64_t min = pow(10, n - 1);
+    uint64_t max = pow(10, n) - 1;
+    return RANDOM(min, max);
+}
+
+uint64_t naivePrimeGeneration(int n) {
+    uint64_t p = generateRandomNumber(n);
+
+    if (p % 2 == 0) { // If p is even, increment it by 1 (to make it odd)
+        p++;
+    }
+
+    while (true) {
+        uint64_t j = 3; // Potential divisor of p
+
+        // Check for divisors up to √p
+        while (j <= sqrt(p)) {
+            if (p % j == 0) { // Check divisibility
+                break;  //Found a divisor
+            }
+            j += 2; // Check the next odd number
+        }
+
+        if (j > sqrt(p)) { // No potential divisor found for p
+            return p;  // Return the prime number
+        }
+
+        p += 2; // Check the next odd number
+    }
+}
+
 int main() {
     srand(time(0));
 
@@ -28,6 +60,10 @@ int main() {
 
     uint64_t randomNumber = RANDOM(a, b);
     cout << randomNumber;
+
+    uint64_t prime = naivePrimeGeneration(n);
+
+    cout << "\nGenerated primer number: " << prime << endl;
 
 
     return 0;
